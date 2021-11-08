f9b7017648052499d15abb9ff35e499832976cb6
AditiKandhway <aditikandhway1234@gmail.com>
2021-11-08 13:10:40 +0530

    Added Background

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="container1">
-    <NavBar></NavBar>
-    <WeatherTemperature></WeatherTemperature>
+    <div class="components">
+      <NavBar></NavBar>
+      <WeatherTemperature></WeatherTemperature>
+    </div>
   </div>
 </template>
 
@@ -25,14 +27,19 @@ export default{
 }
 
 .container1{
-  background-image: url('./assets/background.jpg');
   margin-top:10px;
-  border:5px solid rgb(250, 229, 229);
   border-radius:10px;
   width:700px;
-  box-shadow:5px 5px 5px 5px rgb(250, 229, 229);
   margin-left:auto;
   margin-right:auto;
   box-sizing: border-box;
+}
+.components{
+  background-image: url('./assets/background.jpg');
+  background-repeat: no-repeat;
+  background-size: cover;
+  border-radius:10px;
+  border:5px solid rgb(250, 229, 229);
+  box-shadow:3px 3px 3px 3px rgb(250, 229, 229);
 }
 </style>
